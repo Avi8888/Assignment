570868632e0eca7ddf61905f83316824e5cfee8a
AVINSH <AVINSH@LAPTOP-M7E66H8V>
2021-05-01 14:55:08 +0530

    May MAven

---
+++ MayMAven/src/test/java/Maven_May/MayMAven/Test_1.java
@@ -0,0 +1,61 @@
+package Maven_May.MayMAven;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test_1 {
+
+@BeforeClass
+      public void BC()
+      {
+	    System.out.println("----------Before Class Running");
+      }
+@BeforeMethod
+	public void BM()
+	{
+	System.out.println("------------Before Method Running");
+	}
+
+@BeforeTest
+
+public void BT()
+{
+System.out.println("------------Before Test Running");
+}
+@AfterTest
+
+public void AT()
+{
+System.out.println("------------After Test Running");
+}
+@Test
+    public void Test1()
+   {
+	System.out.println("----------Test-1 Running");
+    }
+
+@Test
+public void Test2()
+{
+System.out.println("----------Test-2 Running");
+}
+
+
+@AfterMethod
+   public void AM()
+   {
+	System.out.println("----------After Method Running");
+    }
+@AfterClass
+   public void AC()
+  {
+	System.out.println("-----------After Class Running");
+  }
+	
+	
+}
